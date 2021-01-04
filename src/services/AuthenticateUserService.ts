ed53194119a9c35966b2efd4084c6afd4e6c56cc
hmartiins <hmartins224@gmail.com>
2021-01-04 03:21:05 -0300

    dealing with errors globally

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,11 +1,11 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
-
 import authConfig from '../config/auth';
 
+import AppError from '../errors/AppError';
+
 import User from '../models/User';
-import auth from '../config/auth';
 
 interface Request {
   email: string;
@@ -22,13 +22,13 @@ export default class AuthenticateUserService {
     const user = await usersRepository.findOne({ where: { email } });
 
     if (!user) {
-      throw new Error('Incorrect email/password combination');
+      throw new AppError('Incorrect email/password combination', 401);
     }
 
     const passwordMatched = await compare(password, user.password);
 
     if (!passwordMatched) {
-      throw new Error('Incorrect email/password combination');
+      throw new AppError('Incorrect email/password combination', 401);
     }
 
     const token = sign({}, authConfig.jwt.secret, {
--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -3,6 +3,8 @@ import { hash } from 'bcryptjs';
 
 import User from '../models/User';
 
+import AppError from '../errors/AppError';
+
 export interface Request {
   name: string;
   email: string;
@@ -18,7 +20,7 @@ export default class CreateUserService {
     });
 
     if (checkUserExists) {
-      throw new Error('Email address already used.');
+      throw new AppError('Email address already used.');
     }
 
     const hashedPassword = await hash(password, 7);
