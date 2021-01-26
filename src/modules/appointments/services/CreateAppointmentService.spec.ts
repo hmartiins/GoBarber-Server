83b1cedf4a45b2a89a338c13dac90d823d6b4028
hmartiins <hmartins224@gmail.com>
2021-01-26 17:11:06 -0300

    creating user creation test

---
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,19 @@
+import FakeAppointmentRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentRepository();
+    const CreateAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await CreateAppointment.execute({
+      date: new Date(),
+      provider_id: '123123123',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123123123');
+  });
+});
