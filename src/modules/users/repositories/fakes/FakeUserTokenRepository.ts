52ba674293710da01a115fc592d707312bb4f24d
hmartiins <hmartins224@gmail.com>
2021-01-31 00:43:28 -0300

    creating test and test for rest password

--- src/modules/users/repositories/fakes/FakeUserTokenRepository.ts
+++ src/modules/users/repositories/fakes/FakeUserTokenRepository.ts
@@ -19,4 +19,12 @@ export default class FakeUserTokensRepository implements IUserTokenRepository {
 
     return userToken;
   }
+
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    const userToken = this.userTokens.find(
+      findToken => findToken.token === token,
+    );
+
+    return userToken;
+  }
 }
---
+++ src/modules/users/services/ResetPasswordService.spec.ts
@@ -0,0 +1,40 @@
+// import AppError from '@shared/errors/AppError';
+
+import FakeUserRepository from '../repositories/fakes/FakeUsersRepository';
+import FakeUserTokensRepository from '../repositories/fakes/FakeUserTokenRepository';
+import ResetPasswordService from './ResetPasswordService';
+
+let fakeUsersRepository: FakeUserRepository;
+let fakeUserTokensRepository: FakeUserTokensRepository;
+let resetPasswordService: ResetPasswordService;
+
+describe('SendForgotPasswordEmail', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUserRepository();
+    fakeUserTokensRepository = new FakeUserTokensRepository();
+
+    resetPasswordService = new ResetPasswordService(
+      fakeUsersRepository,
+      fakeUserTokensRepository,
+    );
+  });
+
+  it('should be able to reset the password', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    const { token } = await fakeUserTokensRepository.generate(user.id);
+
+    await resetPasswordService.execute({
+      password: '123123',
+      token,
+    });
+
+    const updatedUser = await fakeUsersRepository.findById(user.id);
+
+    expect(updatedUser?.password).toBe('123123');
+  });
+});
